a466605e888d2a12da903db936a362edac780845
YaoZhang0916 <38364374+YaoZhang0916@users.noreply.github.com>
2018-06-23 16:50:53 -0500

    first commit

--- JavaScript/disappearingNinja/js/disNinjia.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('img').click(function () {
-        $(this).css('visibility','hidden');
-    })
-
-    $('input').click(function () {
-        $('img').css('visibility','visible');
-    })
-})
